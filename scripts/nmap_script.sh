a12d4328d5a78ce4169932e7d1696ecdc4d8fefa
eneroh <28052446+eneroh@users.noreply.github.com>
2024-04-18 20:15:56 +1000

    Delete scripts directory

--- scripts/nmap_script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#requires root
-
-for ip in $(cat iplist.txt); do nmap -sS -p 80 -T4 $ip; done
--- scripts/records.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-Priv esc docs
-
-Ways to improve a terminal through reverse shell & priv esc etc.
-
-Will place python bin bash command here as well
