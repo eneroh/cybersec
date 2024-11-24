8029b27e708052b8023902c1726fba15addf1c28
eneroh <28052446+eneroh@users.noreply.github.com>
2024-11-24 11:16:56 +1100

    Update records.sh

--- red/scripts/records.sh
+++ red/scripts/records.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+=== records ===
+
+--- python shell to tty shell - pty module ---
+python3 -c 'import pty; pty.spawn("/bin/bash")'
+
 Priv esc docs
 
 Ways to improve a terminal through reverse shell & priv esc etc.
